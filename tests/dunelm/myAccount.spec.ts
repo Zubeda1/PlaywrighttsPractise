c729380ca67fcd6fc7fc5d3cdea0532b03b0d112
Zubeda Shahnan <zshahnan@dunelm.com>
2026-08-12 15:15:58 +0100

    Added more assertion

--- tests/dunelm/myAccount.spec.ts
+++ tests/dunelm/myAccount.spec.ts
@@ -4,5 +4,8 @@ test.describe('My Account', () => {
     test('should display account page', async ({ page }) => {
     await page.goto('https://dunelm.com/product/egyptian-cotton-towel-1000248815?defaultSkuId=30924008')
     await expect(page.getByRole('heading', { name: 'Egyptian Cotton Towel' })).toBeVisible()
+    await page.getByRole('button', { name: 'My Account' }).click()
+    await expect(page.getByRole('heading', { name: 'Hi zubeda' })).toBeVisible()
+
     })
 })
