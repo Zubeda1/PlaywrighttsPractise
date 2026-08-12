8afdf2dca57b67b454c9e8cdd5f742162f2caf50
Zubeda1 <47689756+Zubeda1@users.noreply.github.com>
2026-08-12 11:59:28 +0100

    fix test assertion

--- tests/dunelm/myAccount.spec.ts
+++ tests/dunelm/myAccount.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test'
 test.describe('My Account', () => {
     test('should display account page', async ({ page }) => {
     await page.goto('https://dunelm.com/product/egyptian-cotton-towel-1000248815?defaultSkuId=30924008')
-    await expect(page.getByRole('heading', { name: 'Egyptian Cotton Towel' }).first).toBeVisible()
+    await expect(page.getByRole('heading', { level: 1, name: 'Egyptian Cotton Towel' })).toBeVisible()
     })
 })
