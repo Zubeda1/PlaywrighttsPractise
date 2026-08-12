59e21be20394029a59847f13ee44c4c2a525c14f
Zubeda1 <47689756+Zubeda1@users.noreply.github.com>
2026-08-12 11:54:22 +0100

    fix failing test

--- tests/dunelm/myAccount.spec.ts
+++ tests/dunelm/myAccount.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test'
 test.describe('My Account', () => {
     test('should display account page', async ({ page }) => {
     await page.goto('https://dunelm.com/product/egyptian-cotton-towel-1000248815?defaultSkuId=30924008')
-    await expect(page.getByRole('heading', { name: 'Egyptian Cotton Towel' })).toBeVisible()
+    await expect(page.getByRole('heading', { name: 'Egyptian Cotton Towel' }).first).toBeVisible()
     })
 })
